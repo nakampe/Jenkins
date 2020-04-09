6738fafed386f390672ab9fe0e09063140b86d5b
nakampe <nakmanyama@gmail.com>
2020-04-09 21:31:48 +0200

    Added a simple class for testing purposes

---
+++ src/main/java/co/za/nakampe/wwwdot/Greeting.java
@@ -0,0 +1,21 @@
+package co.za.nakampe.wwwdot;
+
+public class Greeting{
+
+private String message;
+
+public Greeting(String message){
+this.message= message;
+}
+
+public Greeting(){}
+
+public String getMessage(){
+  return message;
+}
+
+public void setMessage(String message){
+this.message=message;
+}
+
+}
---
+++ src/test/java/co/za/nakampe/wwwdot/GreetingTest.java
@@ -0,0 +1,15 @@
+package co.za.nakampe.wwwdot;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class GreetingTest{
+
+@Test
+public void shouldSetMessage(){
+
+Greeting greeting = new Greeting("Dumela");
+
+}
+
+}
